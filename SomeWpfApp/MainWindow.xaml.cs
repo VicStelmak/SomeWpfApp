192d53687c2eb6ed48164995c7db46fdb6cd09a5
VicStelmak <vstelmak@protonmail.com>
2025-03-22 10:30:40 +0300

    Add Configured WPF application

---
+++ SomeWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+
+namespace SomeWpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int _counter = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            DataContext = new SomeViewModel();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _counter++;
+
+            TextBlock1.Text = _counter.ToString();
+        }
+
+        protected override void OnClosed(EventArgs arguments)
+        {
+            base.OnClosed(arguments);
+
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ SomeWpfApp/SomeSource.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Threading;
+
+namespace SomeWpfApp
+{
+    internal class SomeSource
+    {
+        private string _output;
+        private readonly Dispatcher _dispatcher = Application.Current.Dispatcher;
+
+        internal string GetData()
+        {
+            _dispatcher.BeginInvoke(new Action(() => 
+            { 
+                var someWindow = new SomeWindow();
+                someWindow.Closed += (input, eventArgs) =>
+                {
+                    _output = someWindow.UserInput;
+                    
+                    if (Application.Current.MainWindow != null)
+                    {
+                        var currentDataContext = Application.Current.MainWindow.DataContext;
+                        var viewModel = (SomeViewModel)currentDataContext;
+
+                        _dispatcher.Invoke(() =>
+                        {
+                            var mainWindow = Application.Current.MainWindow;
+                            var property = viewModel.GetType().GetProperty("Data");
+
+                            if (property != null) property.SetValue(viewModel, _output);
+
+                            var textBlockToUpdate = "TextBlock2";
+                            var manualBinding = BindingOperations.GetBindingExpression(mainWindow.FindName(textBlockToUpdate)
+                                as TextBlock, TextBlock.TextProperty);
+
+                            manualBinding?.UpdateTarget();
+                        });
+
+                        if (currentDataContext != null)
+                        {
+                            if (Application.Current.Windows.Count == 0) Application.Current.Shutdown();
+                        }
+                    }
+                };
+
+                someWindow.Show();
+            }));
+
+            while (_output == null) 
+            {
+                var currentlyOpenedWindows = Application.Current?.Windows;
+
+                if (currentlyOpenedWindows == null || currentlyOpenedWindows.Count == 0) return "";
+
+                var frame = new DispatcherFrame();
+                _dispatcher.BeginInvoke(DispatcherPriority.Background, new DispatcherOperationCallback(LeaveFrame), frame);
+                Dispatcher.PushFrame(frame);
+            }
+
+            return _output;
+        }
+
+        private object LeaveFrame(object frame)
+        {
+            ((DispatcherFrame) frame).Continue = false; 
+            
+            return null;
+        }
+    }
+}
